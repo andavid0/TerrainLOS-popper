ff7ae2897cd96584889e86d9ffa4af6ec2aa2821
andavid <andavid@ucsc.edu>
2018-03-22 00:32:37 -0700

    update run.sh

--- pipelines/experimental-connectivity/run.sh
+++ pipelines/experimental-connectivity/run.sh
@@ -3,8 +3,11 @@
 # This file should contain the series of steps that are required to execute
 # the pipeline. Any non-zero exit code will be interpreted as a failure
 # by the 'popper check' command
-acvs='10 20 30 40 50 60 70 80 90 100'
-pops='1 10 30 80'
+
+#acvs='10 20 30 40 50 60 70 80 90 100'
+#pops = '1 10 30 80'
+acvs='10'
+pops='1'
 
 for acv in $acvs; do
   for pop in $pops; do
@@ -12,7 +15,7 @@ for acv in $acvs; do
       -v `pwd`:/experiment \
       --workdir=/experiment/Connectivity \
       --entrypoint=python \
-      sbungartz/cooja \
-        calc_experimental_connectivity.py 628 $acv /home/contiki/cooja $pop
+      cooja:2.7 \
+        calc_experimental_connectivity.py 628 $acv /root/contiki $pop
   done
 done
