f12708302fa58874707f034455a4c549b90b627d
andavid <andavid@ucsc.edu>
2018-04-05 21:19:04 -0700

    experimental run

--- pipelines/experimental-connectivity/run.sh
+++ pipelines/experimental-connectivity/run.sh
@@ -5,17 +5,20 @@
 # by the 'popper check' command
 
 #acvs='10 20 30 40 50 60 70 80 90 100'
+acvs='10 20'
 #pops = '1 10 30 80'
 #acvs='10'
 #pops='1'
 
-#for acv in $acvs; do
+for acv in $acvs; do
  # for pop in $pops; do
-  #  docker run --rm \
-   #   -v `pwd`:/experiment \
-    #  --workdir=/experiment/Connectivity \
-     # --entrypoint=python \
-      #cooja:2.7 \
-       # calc_experimental_connectivity.py 628 $acv /root/contiki $pop
+     docker run --rm \
+     -v `pwd`:/experiment \
+     --workdir=/experiment/Connectivity \
+     --entrypoint=python \
+     cooja:2.7 \
+     calc_experimental_connectivity.py 628 $acv /root/contiki 1 >> pop_1_log.txt
  # done
-#done
+done
+
+
